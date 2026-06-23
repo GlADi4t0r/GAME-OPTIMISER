961296f37da3c722ade3fe62b0bf7c71decf353a
Midnight <87692465+GlADi4t0r@users.noreply.github.com>
2026-06-23 17:49:34 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.realme.fpsbooster"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.realme.fpsbooster"
+        minSdk = 26
+        targetSdk = 22 
+        versionCode = 2
+        versionName = "2.5-OPTIMIZED"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
